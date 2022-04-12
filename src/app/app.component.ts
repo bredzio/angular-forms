3bbbe4f702baedc546432d206183ced32dfe8a89
Bruno Redzio <brredzio@emeal.nttdata.com>
2022-04-11 23:58:27 -0300

    Se realizó configuración de reactive form y se agregarón validaciones solicitadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  public myForm: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.myForm = this.fb.group({
+      name:['', Validators.required],
+      age:['', Validators.compose([Validators.required, Validators.max(180)])],
+      company:['', Validators.required],
+      mail:['', Validators.compose([Validators.required, Validators.email])]
+    })
+  }
+
+  sendToService(){
+    console.log(this.myForm.value);
+  }
 }
